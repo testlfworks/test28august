83aa03b8b88534bf68eac227a443dfda2e9af356
LifeWorks <candidate@LifeWorkss-MacBook-Pro.local>
2018-08-28 11:21:31 +0100

    first add

---
+++ vars.tf
@@ -0,0 +1,18 @@
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-west-2 = "ami-06b94666"
+  }
+}
+variable "ENV" {
+  default = "DEV"
+}
